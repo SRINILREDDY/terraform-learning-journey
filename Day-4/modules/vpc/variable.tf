54e66da49e632f75c81bfda7dbb53bdcbd8a7710
Srinil Reddy Alur <138554879+SRINILREDDY@users.noreply.github.com>
2026-08-09 12:05:05 +0530

    Update variable.tf

--- Day-4/modules/vpc/variable.tf
+++ Day-4/modules/vpc/variable.tf
@@ -5,11 +5,11 @@ variable "vpc_cidr_block" {
 
 variable "public_subnet_cidr"{
     type = string
-    default ="10.0.1.0/16"
+    default ="10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
     type = string
-    default ="10.0.2.0/16"
+    default ="10.0.2.0/24"
 }
 
